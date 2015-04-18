ca81907210911d0a702c17c69d03c7382fc551c0
Maxime Vidori <maxime.vidori@gmail.com>
2015-04-18 23:08:11 +0200

    Display the depute data on click

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 $( document ).ready(function() {
-  $('.circo').click(function (e) {
-    console.log($(this).attr('id'));
-    $('<h1>Hello World</h1>').appendTo('body').modal();
+
+  var deputes_promise = $.when($.ajax('/data/deputes.json'));
+
+  deputes_promise = deputes_promise.then(function (data) {
+    var deputes = {};
+    $.each(data.deputes, function(index, depute) {
+      var circo = deputes[depute.depute.num_deptmt] || {};
+      circo[depute.depute.num_circo] = depute.depute;
+      deputes[depute.depute.num_deptmt] = circo;
+    });
+    return deputes;
   });
+
+
+  $('.circo').click(function (e) {
+    var id = $(this).attr('id').split('-'),
+      dept = parseInt(id[0]), circ = parseInt(id[1]);
+
+    deputes_promise.then(function (data) {
+      $('<div>' + JSON.stringify(data[dept][circ]) + '</div>').appendTo('body').modal();
+    });
+  });
+
+
 });
