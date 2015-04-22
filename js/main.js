3f5a74eba1f73a61b08589967e6f8b3da29fa801
Maxime Vidori <maxime.vidori@gmail.com>
2015-04-22 11:45:37 +0200

    Add a link to the depute picture

--- js/main.js
+++ js/main.js
@@ -1,23 +1,31 @@
 $( document ).ready(function() {
-
+  var url_nosdeputes_photo = 'http://www.nosdeputes.fr/depute/photo/';
   var deputes_promise = $.when($.ajax('/data/deputes.json'));
 
   deputes_promise = deputes_promise.then(function (data) {
     var deputes = {};
     $.each(data.deputes, function(index, depute) {
-      var circo = deputes[depute.depute.num_deptmt] || {};
-      circo[depute.depute.num_circo] = depute.depute;
-      deputes[depute.depute.num_deptmt] = circo;
+      depute = depute.depute; // bad API design Yay!
+
+      var circo = deputes[depute.num_deptmt] || {};
+
+      depute.photo_url = url_nosdeputes_photo + depute.slug + '/160'
+      circo[depute.num_circo] = depute;
+      deputes[depute.num_deptmt] = circo;
     });
     return deputes;
   });
 
   $('.circo').click(function (e) {
     var id = $(this).attr('id').split('-'),
-      dept = parseInt(id[0]), circ = parseInt(id[1]);
+      dept = id[0].toUpperCase(), circ = parseInt(id[1]);
+
+    // need to trim starting 0 if exists
+    dept = dept[0] === '0' && dept.length === 3 ? dept.slice(1, 3): dept;
 
     deputes_promise.then(function (deputes){
-      $('#depute').render(deputes[dept][circ]).appendTo('body').modal();
+      var depute = deputes[dept][circ];
+      $('#depute').render(depute).appendTo('body').modal();
     });
   });
 });
