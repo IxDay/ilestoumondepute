8de7868cbc0225df969ad54e14dcf063cd8a07dd
Adèle Beauchamp <adelebeauchamp@macbook-air-de-adele.home>
2015-05-03 16:44:18 +0200

    images, pictos, modale depute

--- js/main.js
+++ js/main.js
@@ -18,13 +18,35 @@ $( document ).ready(function() {
 
   $('.circo').click(function (e) {
     var id = $(this).attr('id').split('-'),
-      dept = id[0].toUpperCase(), circ = parseInt(id[1]);
+      dept = id[0].toUpperCase(), circ = parseInt(id[1]),
+      nom_dept = $(this).children('title').html();
+
+    // TODO : rendre dynamique ( absent || abstention || pour || contre )
+    var vote_PJLR = "absent";
 
     // need to trim starting 0 if exists
     dept = dept[0] === '0' && dept.length === 3 ? dept.slice(1, 3): dept;
 
     deputes_promise.then(function (deputes){
-      var depute = deputes[dept][circ];
+      var depute = deputes[dept][circ],
+        email = depute.emails[0].email,
+        couleur = '#962E27';
+
+      if(vote_PJLR === "absent") {
+        var message_vote = "Ce deputé était <span style='color:" + couleur + "'>absent</span> lors du vote du 14 avril"; 
+
+      } else if(vote_PJLR === "abstention") {
+        var message_vote = "Ce deputé <span  style='color:" + couleur + "'>s'est abstenu</span> lors du vote du 14 avril"; 
+      } else {
+        var message_vote = "Ce deputé <span  style='color:" + couleur + "'>a voté " + vote_PJLR +"</span> lors du vote du 14 avril"; 
+      }
+
+      depute.nom_dept = nom_dept;
+      depute.vote_PJLR = vote_PJLR;
+      depute.message_vote = message_vote;
+      depute.displayMail = email;
+      depute.couleur = couleur;
+
       $('#depute').render(depute).appendTo('body').modal();
     });
   });
