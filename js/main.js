66fb83f125425f413f311443782455c7b637d019
Maxime Vidori <maxime.vidori@gmail.com>
2015-05-04 08:41:18 +0200

    Refactoring and history support

--- js/main.js
+++ js/main.js
@@ -1,55 +1,129 @@
 $( document ).ready(function() {
+  var history = window.history;
+  var location = window.location;
   var url_nosdeputes_photo = 'http://www.nosdeputes.fr/depute/photo/';
   var deputes_promise = $.when($.ajax('/data/deputes.json'));
 
+
+  var depute_email = function (depute) {
+    var email = depute.emails[0];
+    if (email)
+      email = email.email;
+    else
+      email = [
+        depute.prenom[0].toLowerCase(), depute.nom.toLowerCase(),
+        '@assemblee-nationale.fr'
+      ].join('');
+
+    return email;
+  }
+
   deputes_promise = deputes_promise.then(function (data) {
     var deputes = {};
-    $.each(data.deputes, function(index, depute) {
+    $.each(data.deputes, function (_, depute) {
+      var circo, email;
+
       depute = depute.depute; // bad API design Yay!
+      circo = deputes[depute.num_deptmt] || {};
 
-      var circo = deputes[depute.num_deptmt] || {};
+      depute.url = '/#' + depute.slug;
+      depute.photo_url = url_nosdeputes_photo + depute.slug + '/160';
+      depute.email = depute_email(depute);
 
-      depute.photo_url = url_nosdeputes_photo + depute.slug + '/160'
       circo[depute.num_circo] = depute;
       deputes[depute.num_deptmt] = circo;
+    });
+    $('.circo').each(function (_, circo) {
+      getDepute(circo.id).then(function (depute) {
+        if (!depute) return;
+        depute.nom_dept = $(circo).children('title').html();
+      });
     });
     return deputes;
   });
 
-  $('svg').svgPan('France');
+  function getDepute (id) {
+    var dept, circ;
+    id = id.split('-');
 
-  $('.circo').click(function (e) {
-    var id = $(this).attr('id').split('-'),
-      dept = id[0].toUpperCase(), circ = parseInt(id[1]),
-      nom_dept = $(this).children('title').html();
-
-    // TODO : rendre dynamique ( absent || abstention || pour || contre )
-    var vote_PJLR = "absent";
+    dept = id[0].toUpperCase();
+    circ = parseInt(id[1]);
 
     // need to trim starting 0 if exists
     dept = dept[0] === '0' && dept.length === 3 ? dept.slice(1, 3): dept;
 
-    deputes_promise.then(function (deputes){
-      var depute = deputes[dept][circ],
-        email = depute.emails[0].email,
-        couleur = '#962E27';
-
-      if(vote_PJLR === "absent") {
-        var message_vote = "Ce deputé était <span style='color:" + couleur + "'>absent</span> lors du vote du 14 avril"; 
-
-      } else if(vote_PJLR === "abstention") {
-        var message_vote = "Ce deputé <span  style='color:" + couleur + "'>s'est abstenu</span> lors du vote du 14 avril"; 
-      } else {
-        var message_vote = "Ce deputé <span  style='color:" + couleur + "'>a voté " + vote_PJLR +"</span> lors du vote du 14 avril"; 
-      }
-
-      depute.nom_dept = nom_dept;
-      depute.vote_PJLR = vote_PJLR;
-      depute.message_vote = message_vote;
-      depute.displayMail = email;
-      depute.couleur = couleur;
-
-      $('#depute').render(depute).appendTo('body').modal();
+    return deputes_promise.then(function (deputes){
+      return deputes[dept][circ];
     });
+  }
+
+  function displayModal(depute) {
+    var couleur = '#962E27';
+
+    // TODO : rendre dynamique ( absent || abstention || pour || contre )
+    var vote_PJLR = "absent";
+
+    if(vote_PJLR === "absent") {
+      var message_vote = "Ce deputé était <span style='color:" + couleur + "'>absent</span> lors du vote du 14 avril";
+
+    } else if(vote_PJLR === "abstention") {
+      var message_vote = "Ce deputé <span  style='color:" + couleur + "'>s'est abstenu</span> lors du vote du 14 avril";
+    } else {
+      var message_vote = "Ce deputé <span  style='color:" + couleur + "'>a voté " + vote_PJLR +"</span> lors du vote du 14 avril";
+    }
+
+    depute.vote_PJLR = vote_PJLR;
+    depute.message_vote = message_vote;
+    depute.couleur = couleur;
+
+    var modal = $('#depute').render(depute).appendTo('body')
+    modal.on($.modal.OPEN, function () {
+      if (history) history.pushState(depute, '', depute.url);
+    });
+    modal.on($.modal.CLOSE, function () {
+      if (history) history.pushState({}, '', '/');
+    });
+    modal.modal();
+
+  }
+
+  $('.circo').click(function () {
+   getDepute(this.id).then(displayModal);
   });
+
+  // load zoom and pane on svg
+  $('svg').svgPan('France');
+
+  // if history and state load the modal
+  if (history && history.state)
+    displayModal(history.state);
+  //else if (location && location.hash)
+
 });
+
+
+/*
+switch (new Date().getDay()) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+        break;
+}
+*/
