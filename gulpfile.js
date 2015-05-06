4f832c2aa545d00632f49ebfb490f8ed27f4104e
Maxime Vidori <maxime.vidori@gmail.com>
2015-05-06 16:52:23 +0200

    Fix some typos

--- gulpfile.js
+++ gulpfile.js
@@ -38,27 +38,29 @@ gulp.task('clean', function () {
 
 
 gulp.task('index', function () {
-  var jsFilter = $.filter('**/*.js');
   gulp.src('index.html')
     .pipe($.fileInclude({prefix: '@@', basepath: '@file'}))
     .pipe($.useref.assets())
-    .pipe(jsFilter)
-    .pipe($.uglify())
-    .pipe(jsFilter.restore())
+    .pipe($.if('*.js', $.uglify()))
+    .pipe($.if('*.css', $.minifyCss()))
     .pipe($.useref.restore())
     .pipe($.useref())
     .pipe(gulp.dest('./dist/'));
 });
 
+gulp.task('jquery', function () {
+  return gulp.src('./js/vendor/jquery-1.9.1.min.js')
+    .pipe(gulp.dest('./dist/js'));
+});
 
 gulp.task('build', function (cb) {
   var runSequence = require('run-sequence');
 
-  return runSequence('clean', ['img', 'data', 'css'], 'index', cb);
+  return runSequence('clean', ['img', 'data', 'jquery'], 'index', cb);
 });
 
 gulp.task('data', function () {
-  return gulp.src('./data/**').pipe(gulp.dest('./dist/data'));
+  return gulp.src('./data/deputes-vote.json').pipe(gulp.dest('./dist/data'));
 });
 
 gulp.task('fileinclude', function () {
